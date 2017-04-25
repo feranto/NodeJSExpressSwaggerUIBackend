66d748f5894ce797873ea62187ce5d7f4d442ecb
Fernando Mejia <femeji@microsoft.com>
2017-04-25 04:19:18 -0300

    customizacion de código para leer repositorio local de datos

--- listaContactos/server.js
+++ listaContactos/server.js
@@ -1,28 +1,31 @@
 'use strict';
 
-var Http = require('http');
-var Express = require('express');
-var BodyParser = require('body-parser');
-var Swaggerize = require('swaggerize-express');
-var Path = require('path');
+ var port = process.env.PORT || 8000; // first change
 
-var App = Express();
+ var http = require('http');
+ var express = require('express');
+ var bodyParser = require('body-parser');
+ var swaggerize = require('swaggerize-express');
+ var swaggerUi = require('swaggerize-ui'); // second change
+ var path = require('path');
 
-var Server = Http.createServer(App);
+ var app = express();
 
-App.use(BodyParser.json());
-App.use(BodyParser.urlencoded({
-    extended: true
-}));
+ var server = http.createServer(app);
 
-App.use(Swaggerize({
-    api: Path.resolve('./config/swagger.json'),
-    handlers: Path.resolve('./handlers')
-}));
+ app.use(bodyParser.json());
 
-Server.listen(8000, function () {
-    App.swagger.api.host = this.address().address + ':' + this.address().port;
-    /* eslint-disable no-console */
-    console.log('App running on %s:%d', this.address().address, this.address().port);
-    /* eslint-disable no-console */
-});
+ app.use(swaggerize({
+     api: path.resolve('./config/swagger.json'), // third change
+     handlers: path.resolve('./handlers'),
+     docspath: '/swagger' // fourth change
+ }));
+
+ // change four
+ app.use('/docs', swaggerUi({
+   docs: '/swagger'  
+ }));
+
+ server.listen(port, function () { // fifth and final change
+ });
+ 
